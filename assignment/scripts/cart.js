89ea00673d79db9afbc1d1d10d9bbebb90349638
Julia Bayless <julia.allistair@gmail.com>
2021-09-10 15:00:00 -0500

    added items to basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,5 +2,18 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = []
+let basket = [] //creating empty array
+console.log(basket); //checking array works
+
+function addItem( item ) {
+  console.log( 'Adding item to basket:', item);
+  basket.push( item );
+  return true;
+}
+
+addItem( 'apple' );
+console.log(basket);
+addItem( 'sandwhich' );
+addItem( 'silverware' );
+addItem( 'napkin' );
 console.log(basket);
